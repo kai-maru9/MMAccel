099afafb44b7abf06748630ea00d52e7818a3a14
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2021-05-02 20:00:19 +0900

    依存関係のライセンスを追加

--- mmaccel/src/template_package.ps1
+++ mmaccel/src/template_package.ps1
@@ -6,5 +6,6 @@ Copy-Item "target/release/mmaccel.dll" $mmaccel_dir
 Copy-Item "target/release/key_config.exe" $mmaccel_dir
 Copy-Item "mmaccel/src/mmd_map.json" $mmaccel_dir
 Copy-Item "key_config/src/order.json" $mmaccel_dir
+Copy-Item -Recurse "licenses" $mmaccel_dir
 
 Compress-Archive -Force -Path $dir -DestinationPath "package/MMAccel_{0}"
